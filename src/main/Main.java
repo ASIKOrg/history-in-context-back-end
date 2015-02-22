523a8727397bc0131c172836b52e6df6c756a966
Ian Bantoto <lcbantoto@gmail.com>
2015-02-22 10:56:03 -0500

    Add TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
+/*
+ * TODO: 
+ * TODO: 
+ * TODO: 
+ * TODO: 
+ * TODO: 
+ * TODO: 
+ * TODO: 
+ */
+
 public class Main
 {
-
+	// Global variables
+	static int[] in;
+	static int[] out;
+	
     public static void main(String[] args)
     {
         System.out.println("Hello World");
