7c1be66bce157fc606c52b5955dd4a1f38118a07
kylevedder <kylecvedder@gmail.com>
2015-02-23 12:14:22 -0500

    Added request to freebase.

--- src/dispatcher/Dispatcher.java
+++ src/dispatcher/Dispatcher.java
@@ -4,6 +4,8 @@ import parser.Parser;
 import parser.ParserNew;
 import dates.Date;
 import dates.DateJSON;
+import freebase.FreebaseHTTPRequest;
+import freebase.FreebaseRequest;
 import utils.StringEncoder;
 import wikipedia.WikipediaHTTPRequest;
 import wikipedia.WikipediaRequest;
@@ -11,44 +13,50 @@ import wikipedia.WikipediaRequest;
 public class Dispatcher
 {
     private static Dispatcher dispatcher = null;
-    
+
     /**
      * Singleton instance of dispatcher.
+     * 
      * @return
      */
     public static Dispatcher getInstance()
     {
-        if(dispatcher == null)
+        if (dispatcher == null)
         {
             dispatcher = new Dispatcher();
         }
         return dispatcher;
     }
-    
+
     /**
      * Executes the API and returns the API response data.
+     * 
      * @return
      */
     public String executeAPI(String uri, String inputVal)
-    {        
-        //error check
-        if(inputVal != null)
+    {
+        // error check
+        if (inputVal != null)
         {
             System.out.println("URI:          <" + uri + ">");
             System.out.println("Input Val:    <" + inputVal + ">");
             String cleanInputVal = StringEncoder.decodeGET(inputVal);
             System.out.println("Clean Input:  <" + cleanInputVal + ">");
+
+//            //create wikipedia object
+//            WikipediaRequest wikiReq = new WikipediaRequest(cleanInputVal);
+//            System.out.println("Sending Wiki request to: " + wikiReq);
+//            //execute HTTP request
+//            String pageHTML = WikipediaHTTPRequest.sendGet(wikiReq);
+
+            // create freebase object
+            FreebaseRequest freeReq = new FreebaseRequest(cleanInputVal);
+            // execute HTTP request
+            String pageHTML = FreebaseHTTPRequest.sendGet(freeReq);
+            System.out.println("Page HTML:\n" + pageHTML);
             
-            //create wikipedia object
-            WikipediaRequest wikiReq = new WikipediaRequest(cleanInputVal);
-            System.out.println("Sending Wiki request to: " + wikiReq);
             
-            //execute HTTP request
-            String pageHTML = WikipediaHTTPRequest.sendGet(wikiReq);
-            
-            
-            
-            Date wikiDates = Parser.process(pageHTML);//new Date("10", "20");
+            Date wikiDates = Parser.process(pageHTML);// new Date("10", "20");
             DateJSON dateJSON = new DateJSON(wikiDates);
             return dateJSON.toString();
         }
